d9932d9ec91158b962ae68e2042df7ea276828c9
BlessingAdewale <adelekeblessingadewale@gmail.com>
2024-10-31 14:37:52 +0100

    Altered the babel.config.js

--- lendsqr-fp-news/babel.config.js
+++ lendsqr-fp-news/babel.config.js
@@ -1,6 +1,39 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['module:@react-native/babel-preset']
+    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          extensions: [
+            '.ios.ts',
+            '.android.ts',
+            '.ts',
+            '.ios.tsx',
+            '.android.tsx',
+            '.tsx',
+            '.jsx',
+            '.js',
+            '.json',
+            '.svg',
+          ],
+          alias: {
+            '@assets': './src/assets',
+            '@components': './src/components/index.ts',
+            '@constants': './src/constants/index.ts',
+            '@hooks': './src/hooks/index.ts',
+            '@globalStyles': './src/assets/globalStyles.ts',
+            '@screens': './src/screens/index.ts',
+            '@utils': './src/utils/index.ts',
+            '@state': './src/state/index.ts',
+            '@context': './src/context/index.ts',
+          },
+        },
+      ],
+      'react-native-paper/babel',
+      'react-native-reanimated/plugin',
+    ],
   };
 };
