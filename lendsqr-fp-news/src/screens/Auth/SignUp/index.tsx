989f1d5aab83255cbfeceb186e94339196ded687
BlessingAdewale <adelekeblessingadewale@gmail.com>
2024-10-31 14:30:42 +0100

    Created all the screens needed for the auth and Home

---
+++ lendsqr-fp-news/src/screens/Auth/SignUp/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export const SignUp = () => {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  )
+}
+
+
+
+const styles = StyleSheet.create({})
